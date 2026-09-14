2d0a929031e18b510c1a4eed3c596ba94ca23a7c
wak6817 <b.morris.claus@icloud.com>
2026-09-14 21:14:51 +0200

    Switched to TypeScript

---
+++ assets/sounds/pixelv/sound.ts
@@ -0,0 +1,31 @@
+export {};
+
+const currentScript = document.currentScript;
+const scriptUrl =
+  currentScript instanceof HTMLScriptElement
+    ? currentScript.src
+    : document.baseURI;
+const buttonSfx = new Audio(
+  new URL("./assets/clickbtn.wav", scriptUrl).toString(),
+);
+
+buttonSfx.addEventListener(
+  "error",
+  () => {
+    buttonSfx.src = new URL("./clickbtn.wav", scriptUrl).toString();
+  },
+  { once: true },
+);
+
+document.addEventListener("click", (event: MouseEvent) => {
+  const target = event.target;
+  if (!(target instanceof Element)) {
+    return;
+  }
+
+  const button = target.closest("button");
+  if (button) {
+    buttonSfx.currentTime = 0;
+    buttonSfx.play().catch(console.error);
+  }
+});
